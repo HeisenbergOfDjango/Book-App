22c0fb9a187b2bf432ad119c94f20ba5505a3373
Heisenberg of Django <154004523+HeisenbergOfDjango@users.noreply.github.com>
2024-03-23 03:43:27 +0530

    Create urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth.views import *
+
+
+urlpatterns = [
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="Logout"),
+    path("password_change/", PasswordChangeView.as_view(), name="password_change"),
+    path("password_change_done/", PasswordChangeDoneView.as_view(), name="password_change_done"),
+    path("password_reset/", PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset_done", PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uidb64>/<token>", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done", PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+]
